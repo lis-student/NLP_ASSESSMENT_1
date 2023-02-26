5f307539f87903bf515e90cbd60932210aee3786
lis-student <124005095+lis-student@users.noreply.github.com>
2023-02-26 14:33:07 +0000

    Delete Save Empty CSV.py

--- Save Empty CSV.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-data = pd.DataFrame({'Text': [0], 'String': [0], 'Word': ['Test'], 'Valence': [0], 'Arousal': [0], 'Dominance': None})
-data.to_csv('Harry Potter VAD.csv', index=False)
